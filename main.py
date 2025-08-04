e4324d252badcc4dbbfcfacd2cafb6f6abe6f334
namenaro <nanenaro@gmail.com>
2025-08-04 19:13:46 +0300

    добавлена сущность "задача сессии" - содержит имена левой и правой точек, пути к датасетам и т..д.

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 import tkinter as tk
 
-
 from model import Model
 from controller import Controller
 
+from paths import PATH_TO_MULTITRACKS, PATH_TO_FORMS_DATASETS
 
+class Task:
+    def __init__(self, left_name:str, right_name:str, target_name:str):
+        self.multitrack_filename=None
+        self.form_dataset_filename = None
+
+        self.indices_in_dataset = None
+
+        self.left_name = left_name
+        self.right_name = right_name
+        self.target_name = target_name
+
+
+def create_dammy_task():
+    task = Task(left_name = 'p1', right_name = 'p3', target_name = 'p2')
+    task.indices_in_dataset = [0, 1, 2, 3, 4]
+    task.form_dataset_filename = PATH_TO_FORMS_DATASETS + "\\RS_i.json"
+    task.multitrack_filename = PATH_TO_MULTITRACKS +"\\цц.json"
+    return task
 
 if __name__ == "__main__":
-    from paths import PATH_TO_MULTITRACKS, PATH_TO_FORMS_DATASETS
+    task = create_dammy_task()
 
-    # запрашиваем пути к файлу мультитрека и датасета формы multitrack_filename, form_dataset_filename, indices_in_dataset
-    import easygui
-
-    # Выбор файла через диалоговое окно
-
-    multitrack_filename =  easygui.fileopenbox(title="Выберите файл для редактированя", default=PATH_TO_MULTITRACKS + "/*")#PATH_TO_MULTITRACKS +"\\test_multi.json"
-    form_dataset_filename = PATH_TO_FORMS_DATASETS + "\\RS_i.json"
-    indices_in_dataset = [0, 1, 2, 3, 4]
-    left_name = 'p1'
-    right_name = 'p3'
-    target_name = 'p2'
-
-    model = Model(multitrack_filename=multitrack_filename,
-                  form_dataset_filename=form_dataset_filename,
-                  indices_in_dataset=indices_in_dataset,
-                  left_name=left_name,
-                  right_name = right_name,
-                  target_name=target_name
+    model = Model(multitrack_filename=task.multitrack_filename,
+                  form_dataset_filename=task.form_dataset_filename,
+                  indices_in_dataset=task.indices_in_dataset,
+                  left_name=task.left_name,
+                  right_name = task.right_name,
+                  target_name=task.target_name
                   )
 
     root = tk.Tk()
