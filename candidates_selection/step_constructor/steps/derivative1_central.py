17457498b0db89efa77bf396d613ac4a72196dce
namenaro <nanenaro@gmail.com>
2025-08-27 18:54:29 +0300

    рефакторинг центральной разности

--- candidates_selection/step_constructor/steps/derivative1_central.py
+++ candidates_selection/step_constructor/steps/derivative1_central.py
@@ -9,16 +9,13 @@ class Derivative1_central(Step):
     type_of_step = TYPES_OF_STEP.signal
     comment = ""
 
-    def __init__(self, window_length_int:int = 21, polyorder=2 ):
-        self.polyorder = polyorder
-        self.window_length_int = window_length_int
+    def __init__(self ):
+        pass
 
     def run(self, signal, left:float, right:float):
         if len(signal) <= 1:
             return []
 
-        if len(signal)>self.window_length_int:
-            self.window_length_int = len(signal)
         signal = np.asarray(signal, dtype=float)
         derivative = np.zeros_like(signal)
 
